e98ab51391acb0c648bfcd2ec4cffcbd9267a8b0
TZY <3086856337@qq.com>
2019-12-11 14:47:48 +0800

    1211

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,17 +8,19 @@ go
 if exists(select * from sys.tables where name='UserInfo')
 drop table UserInfo
 go
+--角色表
 create table UserInfo
 (
 	ID int primary key identity(1,1),
 	Name varchar(20) not null,
 	PassWrod varchar(20) not null,
-	spice int default(1) check(spice=1 or spice=2)
+	spice int default(1) check(spice=1 or spice=2 or spice=3)
 )
 
 if exists(select * from sys.tables where name='ShoopInfo')
 drop table ShoopInfo
 go
+--商品信息表
 create table ShoopInfo
 (
 	ShoopID int primary key identity(1,1),
@@ -29,10 +31,13 @@ create table ShoopInfo
 	ShoopTime datetime not null,
 	ID int references UserInfo(ID)
 )
+go
+
+
 
 insert into UserInfo values('张三','123456','1')
-insert into UserInfo values('李四','123456','1')
-insert into UserInfo values('admin','admin','2')
+insert into UserInfo values('李四','123456','2')
+insert into UserInfo values('admin','admin','3')
 
 insert into ShoopInfo values('土豆','1','2','20','2019-12-09','1')
 insert into ShoopInfo values('油菜','2','3','20','2019-12-09','1')
